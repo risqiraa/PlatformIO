82187424bb4286809948e49ba0f4f9141a463f8e
Risqi RA <11.risqi.llah@gmail.com>
2022-07-08 18:45:40 +0700

    awal

---
+++ Projects/TELEBOT v1.0 BASIC/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h> // library arduino
+#include <CTBot.h> // library telegram bot
+
+CTBot myBot;
+
+String ssid  = "HARIINNN"    ; // REPLACE mySSID WITH YOUR WIFI SSID
+String pass  = "00000000"; // REPLACE myPassword YOUR WIFI PASSWORD, IF ANY
+String token = "myToken"   ; // REPLACE myToken WITH YOUR TELEGRAM BOT TOKEN
+
+
+//START VOID GREETING
+void greeting(){ 
+
+}
+//END VOID GREETING
+
+
+//START VOID SETUP
+void setup() {
+  
+}
+//END VOID SETUP
+
+
+//START VOID LOOP
+void loop() {
+  
+}
+//END VOID LOOP
