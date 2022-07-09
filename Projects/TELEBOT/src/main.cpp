a4ee3e6fe68cb479f61e0229e09b387afe770b5c
Risqi RA <11.risqi.llah@gmail.com>
2022-07-09 14:37:42 +0700

    kontrol 6 lampu with nodemcu berbasis telegram bot

--- Projects/TELEBOT/src/main.cpp
+++ Projects/TELEBOT/src/main.cpp
@@ -7,16 +7,18 @@ String ssid  = "HARIINNN"    ; // REPLACE mySSID WITH YOUR WIFI SSID
 String pass  = "00000000"; // REPLACE myPassword YOUR WIFI PASSWORD, IF ANY
 String token = "5424171815:AAGYT8if_KMlM0KrPWr1yDG64oL227IT6D0"   ; // REPLACE myToken WITH YOUR TELEGRAM BOT TOKEN
 
-uint8_t led = 2;            // the onboard ESP8266 LED.    
-                            // If you have a NodeMCU you can use the BUILTIN_LED pin
-                            // (replace 2 with BUILTIN_LED)	
+uint8_t led1 = D1;           
+uint8_t led2 = D2;           
+uint8_t led3 = D3;           
+uint8_t led4 = D5;           
+uint8_t led5 = D6;           
+uint8_t led6 = D7;           
 
 
 //*START VOID SETUP
 void setup() {
 	// initialize the Serial
 	Serial.begin(115200);
-	Serial.println("Starting TelegramBot...");
 
 	// connect the ESP8266 to the desired access point
 	myBot.wifiConnect(ssid, pass);
@@ -30,8 +32,20 @@ void setup() {
 	else
 		Serial.println("\ntestConnection NOK");
 		
-	pinMode(led, OUTPUT);
-	digitalWrite(led, HIGH); // turn off the led (inverted logic!)
+	pinMode(led1, OUTPUT);
+	pinMode(led2, OUTPUT);
+	pinMode(led3, OUTPUT);
+	pinMode(led4, OUTPUT);
+	pinMode(led5, OUTPUT);
+	pinMode(led6, OUTPUT);
+
+	digitalWrite(led1, LOW); 
+	digitalWrite(led2, LOW); 
+	digitalWrite(led3, LOW); 
+	digitalWrite(led4, LOW); 
+	digitalWrite(led5, LOW); 
+	digitalWrite(led6, LOW); 
+	
 }
 //*END VOID SETUP
 
@@ -43,13 +57,71 @@ void loop() {
 	// if there is an incoming message...
 	if (CTBotMessageText == myBot.getNewMessage(msg)) {
 
-		if (msg.text.equalsIgnoreCase("LIGHT ON")) {              // if the received message is "LIGHT ON"...
-			digitalWrite(led, LOW);                               // turn on the LED (inverted logic!)
-			myBot.sendMessage(msg.sender.id, "Light is now ON YEY");  // notify the sender
+		if (msg.text.equalsIgnoreCase("L1 ON")) {              
+			digitalWrite(led1, HIGH);                              
+			myBot.sendMessage(msg.sender.id, "L1 is now ON ");  
 		}
-		else if (msg.text.equalsIgnoreCase("LIGHT OFF")) {        // if the received message is "LIGHT OFF"...
-			digitalWrite(led, HIGH);                              // turn off the led (inverted logic!)
-			myBot.sendMessage(msg.sender.id, "Light is now OFF"); // notify the sender
+		else if (msg.text.equalsIgnoreCase("L1 OFF")) {      
+			digitalWrite(led1, LOW);                              
+			myBot.sendMessage(msg.sender.id, "L1 is now OFF"); 
+		}
+		else if (msg.text.equalsIgnoreCase("L2 ON")) {             
+			digitalWrite(led2, HIGH);                              
+			myBot.sendMessage(msg.sender.id, "L2 is now ON ");  
+		}
+		else if (msg.text.equalsIgnoreCase("L2 OFF")) {       
+			digitalWrite(led2, LOW);                             
+			myBot.sendMessage(msg.sender.id, "L2 is now OFF"); 
+		}
+		else if (msg.text.equalsIgnoreCase("L3 ON")) {           
+			digitalWrite(led3, HIGH);                             
+			myBot.sendMessage(msg.sender.id, "L3 is now ON ");  
+		}
+		else if (msg.text.equalsIgnoreCase("L3 OFF")) {        
+			digitalWrite(led3, LOW);                             
+			myBot.sendMessage(msg.sender.id, "L3 is now OFF"); 
+		}
+		else if (msg.text.equalsIgnoreCase("L4 ON")) {          
+			digitalWrite(led4, HIGH);                              
+			myBot.sendMessage(msg.sender.id, "L4 is now ON ");  
+		}
+		else if (msg.text.equalsIgnoreCase("L4 OFF")) {       
+			digitalWrite(led4, LOW);                            
+			myBot.sendMessage(msg.sender.id, "L4 is now OFF"); 
+		}
+		else if (msg.text.equalsIgnoreCase("L5 ON")) {              
+			digitalWrite(led5, HIGH);                              
+			myBot.sendMessage(msg.sender.id, "L5 is now ON ");  
+		}
+		else if (msg.text.equalsIgnoreCase("L5 OFF")) {        
+			digitalWrite(led5, LOW);                              
+			myBot.sendMessage(msg.sender.id, "L5 is now OFF"); 
+		}
+		else if (msg.text.equalsIgnoreCase("L6 ON")) {             
+			digitalWrite(led6, HIGH);                               
+			myBot.sendMessage(msg.sender.id, "L6 is now ON ");  
+		}
+		else if (msg.text.equalsIgnoreCase("L6 OFF")) {        
+			digitalWrite(led6, LOW);                              
+			myBot.sendMessage(msg.sender.id, "L6 is now OFF"); 
+		}
+		else if (msg.text.equalsIgnoreCase("ALL ON")) {        
+			digitalWrite(led1, HIGH);                              
+			digitalWrite(led2, HIGH);                              
+			digitalWrite(led3, HIGH);                              
+			digitalWrite(led4, HIGH);                              
+			digitalWrite(led5, HIGH);                              
+			digitalWrite(led6, HIGH);                              
+			myBot.sendMessage(msg.sender.id, "ALL Lamp is now OFF"); 
+		}
+		else if (msg.text.equalsIgnoreCase("ALL OFF")) {        
+			digitalWrite(led1, LOW);                              
+			digitalWrite(led2, LOW);                              
+			digitalWrite(led3, LOW);                              
+			digitalWrite(led4, LOW);                              
+			digitalWrite(led5, LOW);                              
+			digitalWrite(led6, LOW);                              
+			myBot.sendMessage(msg.sender.id, "ALL Lamp is now OFF"); 
 		}
 		else {                                                    // otherwise...
 			// generate the message for the sender
@@ -64,16 +136,4 @@ void loop() {
 //*END VOID LOOP
 
 
-//*START VOID CEK KONEKSI
-void cekkoneksi() {
-	
-}
-//*END VOID CEK KONEKSI
 
-
-//*START VOID GREETING
-void greeting(){ 
-  TBMessage msg;
-  myBot.sendMessage(msg.sender.id, "HAI ADMINISTRATOR");
-}
-//*END VOID GREETING
